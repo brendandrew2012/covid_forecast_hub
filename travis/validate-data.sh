43895807364dfa218e2fc809c1c2a17de6a015b2
Katie House <katiehouse3@gmail.com>
2020-05-13 13:52:36 -0400

    clean up visualization branch

--- travis/validate-data.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# validate file names
-echo "TESTING FILENAMES..."
-Rscript code/validation/validate_filenames.R
-
-# test covid forecast submission formatting
-echo "TESTING SUBMISSIONS..."
-python3 code/validation/test-formatting.py
